ba1acaeb681be8a470523515ea47ec393793875c
Ingelin Birkeland Rage <ingelinbr@uia.no>
2026-02-10 10:47:33 +0100

    Add C# library and unit tests

---
+++ MyLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using MyLibrary;
+
+namespace MyLibrary.Tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+        var result = Calculator.Add(2, 3);
+        Assert.Equal(5, result);
+    }
+}
---
+++ MyLibrary/MyLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace MyLibrary;
+
+public static class Calculator
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
